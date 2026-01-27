b98ab4025d93c5633e5396197642ccd322a1ddbc
cagataycemaksoy <cemaksoy7890@gmail.com>
2026-01-27 11:06:29 +0100

    Initial Commit

---
+++ World Explorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  World Explorer
+//
+//  Created by Cem Aksoy on 27.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ World Explorer/World_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  World_ExplorerApp.swift
+//  World Explorer
+//
+//  Created by Cem Aksoy on 27.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct World_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
